2fd9dbdb809743fbc0fbbaab08a0594f3553c46d
audhiaprilliant <audhiaprilliant@gmail.com>
2020-07-23 15:19:22 +0700

    Penalty Shooting-Out II

--- DSA Learning Series/2 Linear Data Structures/2 Penalty Shoot-Out II.py
+++ DSA Learning Series/2 Linear Data Structures/2 Penalty Shoot-Out II.py
@@ -6,12 +6,28 @@ for _ in range(int(input())):
     numInput = int(input())
     listPenalty = [int(num) for num in str(input())][:2 * numInput]
     timA,timB = 0,0
+    shotA,shotB = numInput,numInput
     iterNum = 0
-    for elem in range(1,len(listPenalty),2):
-        if (timA != ((numInput // 2) + 1)) & (timB != ((numInput // 2) + 1)):
-            timA += listPenalty[elem - 1]
-            timB += listPenalty[elem]
-            iterNum = (elem + 1)
+    for elem in range(len(listPenalty)):
+        if elem % 2 == 0:
+            shotA -= 1
+            if listPenalty[elem] == 1:
+                timA += 1
         else:
-            break
+            shotB -= 1
+            if listPenalty[elem] == 1:
+                timB += 1
+        
+        diffGoal = timA - timB
+        if diffGoal > 0:
+            if diffGoal > shotB:
+                iterNum = elem + 1
+                break
+        elif diffGoal < 0:
+            if abs(diffGoal) > shotA:
+                iterNum = elem + 1
+                break
+        else:
+            if elem == 2 * numInput - 1:
+                iterNum = elem + 1
     print(iterNum)
